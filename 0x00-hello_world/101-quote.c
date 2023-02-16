39a285068cfd619d7f958dbf616638d4ab0c54f7
manta97 <122909603+manta97@users.noreply.github.com>
2023-02-16 14:37:40 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: 1 
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	fwrite(2, 'and that piece of art is useful\" - Dora Korpar, 2015-10-19\n', 58);
 
 	return (1);
 }
